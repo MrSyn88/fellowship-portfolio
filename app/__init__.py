1d086441a9f03548d8d3f6af7fa6b7c41b780eb5
carghai <carghai74@gmail.com>
2023-06-23 11:23:10 -0400

    base code added

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About", url=os.getenv("URL"))
+
+
+@app.route('/hobbies')
+def hobbies():
+    return render_template('hobbies.html', title="Hobbies", url=os.getenv("URL"))
+
+@app.route('/education')
+def education():
+    return render_template('education.html', title="Education", url=os.getenv("URL"))
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', title="Projects", url=os.getenv("URL"))
